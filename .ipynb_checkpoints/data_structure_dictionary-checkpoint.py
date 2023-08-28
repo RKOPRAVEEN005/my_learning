0c4e3f24aaf5f28da0a3d24b0b6801244e5a7f77
KBRO <rkopraveen005@gmail.com>
2023-08-28 13:28:10 +0530

    Keyword

---
+++ .ipynb_checkpoints/data_structure_dictionary-checkpoint.py
@@ -0,0 +1,12 @@
+# how to create a dictionary
+
+empty_dict = {}
+
+# sample dictionary
+
+sample_dictionary = {'key': 'value'} # format
+example_dictionary = {'names': ['praveen', 'senthil'], 'qualification': 'msc'}
+
+# mutable or immutable
+
+example_dictionary['names']
